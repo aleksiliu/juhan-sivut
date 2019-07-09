4dc6844d705070ce6d8e6656514a72e0cec212d9
aleksiliu <liukkonenaleksi@gmail.com>
2019-07-09 12:04:17 +0300

    Add grid

--- src/components/about.js
+++ src/components/about.js
@@ -3,16 +3,29 @@ import styled from "styled-components"
 import Container from "./container"
 
 const TextBlock = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 24px 0;
+  margin-top: 40px;
+`
+
+const Block = styled.div`
+  display: grid;
+  grid-template-columns: 280px 2fr;
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
 `
 const H2 = styled.h2`
-  width: 500px;
+  
   ::after {
-    display: inline-block;
+    background-color: #88c577;
     content: "";
-    border-bottom: .3rem solid #88c577;
-    width: 4rem;
-    margin: 1rem 1rem 0 0;
+    display: block;
+    width: 50px;
+    height: 5px;
+    margin-top: 0.8em;
 }
 `
 
@@ -20,12 +33,14 @@ const About = () => (
   
     <Container>
       <TextBlock>
+        <Block>
       <H2>Kuka on Juha?</H2>
       <p>Juha on tiimipelaaja henkeen ja vereen sekä venyy tarvittaessa koko joukkueen puolesta tulosten saavuttamiseksi. Hänellä on myös jatkuva halu oppia ja kehittyä sekä hänen analyyttisyys tuo rutkasti lisäarvoa asiakkaalle.</p>
-      </TextBlock>
-      <TextBlock>
+      </Block>
+     <Block>
       <H2>Mitä Juha osaa?</H2>
       <p>Juha osaa priorisoida tehtävät tärkeysjärjestykseen, jos hän on useassa projektissa mukana. Hän on johtanut useita asiakasprojekteja lunastuksen osalta omaa esimerkkiä näyttäen ja projekteja koordinoiden. Osaamista löytyy muun muassa B2B-myynnin alkupäästä, asiakashaastatteluista sekä asiakaslähtöisestä viestinnästä, raportoinnista ja presentaatioista.</p>
+      </Block>
       </TextBlock>
     </Container>
   
