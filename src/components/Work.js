87793bb6d512dd650513c5c8857a36c257f0ea87
aleksiliu <liukkonenaleksi@gmail.com>
2019-07-12 23:14:36 +0300

    Add work experience

--- src/components/Work.js
+++ src/components/Work.js
@@ -23,9 +23,16 @@ const P = styled.p`
   font-size: 14px;
   margin-bottom: 8px;
 `
+const WorkYear = styled.p`
+  color: #5C5C68;
+`
+
+const WorkExp = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
 
 const Col = styled.div`
-  
   background-color: #FFF; 
   border-radius: 4px;
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
@@ -38,9 +45,11 @@ const Work = () => (
     
         <H2>Työkokemus</H2>
        <Row>
-
            <Col>    
-           <P>TRAINER'S HOUSE</P>
+            <WorkExp>
+                <P>TRAINER'S HOUSE</P>
+                <WorkYear>2016 - 2019</WorkYear>
+           </WorkExp>
            <h2>Change Support Specialist</h2>
            <ul>
             <li>Seniorina yli 10 muutostukiprojektissa liittyen auditointeihin ja PolullaPysymisPuheluihin. Lunastus, koordinointi, tulosten analysointi ja presentaatio asiakkaalle.</li>
@@ -51,7 +60,15 @@ const Work = () => (
            
            </Col>
            <Col>    
-           <p>adsadsa</p>
+           <WorkExp>
+                <P>DANSKE BANK OYJ</P>
+                <WorkYear>2012 - 2012</WorkYear>
+           </WorkExp>
+           <h2>Ammattiharjoittelu/Palvelutiimi</h2>
+           <ul>
+            <li>Asiakkaiden päivittäisten raha-asioiden hoitaminen sekä asiakaspalvelu henkilökohtaisesti ja puhelimessa.</li>
+            <li> Myynnin tuki, kuten asiakastapaamisten sopiminen. Sopimani tapaaminen johti parhaimmillaan yli 100k€ sijoitukseen.</li>
+           </ul>
            
            </Col>
        </Row>
